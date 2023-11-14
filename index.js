290f4e0865efce25541937d6d69435f58bde818d
Vlad Myronenko <orurez111@gmail.com>
2023-11-14 22:34:02 +0200

    dsfsf

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const app = document.querySelector('.app');
+const defaultPath = window.location.href;
 
-const route = (event) => {
-  event.preventDefault();
-  window.history.pushState({}, '', event.target.href);
+const route = (path) => {
+  window.event.preventDefault();
+  let updatePath = `${defaultPath}${path}`;
+  window.history.pushState({}, '', updatePath);
+  localStorage.setItem('loc', path);
   handleLocation();
 };
 
 function handleLocation() {
-  const path = window.location.pathname;
+  const positionSlash = window.location.pathname.lastIndexOf('/');
+  const path = window.location.pathname.slice(positionSlash);
   switch (path) {
     case '/':
       app.innerHTML = `
@@ -29,5 +33,28 @@ function handleLocation() {
   }
 }
 window.onpopstate = handleLocation;
-window.route = route;
+window.onload = () => {};
+// document.addEventListener('DOMContentLoaded', () => {
+//   let lastPage = localStorage.getItem('loc');
+//   console.log(lastPage);
+//   switch (lastPage) {
+//     case '/':
+//       app.innerHTML = `
+//             <h1>Main page </h1>
+//         `;
+//       break;
+
+//     case '/users':
+//       app.innerHTML = `
+//             <h1>users page </h1>
+//         `;
+//       break;
+
+//     case '/posts':
+//       app.innerHTML = `
+//             <h1>posts page </h1>
+//         `;
+//       break;
+//   }
+// });
 handleLocation();
